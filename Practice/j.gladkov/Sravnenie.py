97e09bd6731fc0f9dd5ae2a36e7312a2d1687e07
itqrj1 <40244004+itqrj1@users.noreply.github.com>
2020-02-07 15:15:28 +0300

    Add files via upload

---
+++ Practice/j.gladkov/Sravnenie.py
@@ -0,0 +1,20 @@
+def compare(a, b):
+    if a>=b:
+        print (str(a) + "is bigger")
+    else:
+        print( str(b) + "is bigger")
+x = 155
+y = 13574
+compare(x,y)
+
+print("move on?")
+
+def sravnen(a, b):
+    if a>=b:
+        print (str(a) + "is bigger")
+    else:
+        print( str(b) + "is bigger")
+z = input ( "first value ")
+v = input ( "second value")
+
+sravnen(z,v)
