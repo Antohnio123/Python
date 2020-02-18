9b4d507d61791b16043327566001cb4e8d84bcda
Aleksandr Segeev <sandy.sergeev@mail.ru>
2020-02-18 07:28:43 +0300

    Homework 5

---
+++ Practice/a.sergeev/PyLec_5/Task1.py
@@ -0,0 +1,7 @@
+class Man():
+    def Solve_Task(self):
+        return "Not yet ready"
+
+vivod_Man=[Man()]
+for vivod in vivod_Man:
+    print(vivod.Solve_Task())
---
+++ Practice/a.sergeev/PyLec_5/Task2:2.py
@@ -0,0 +1,21 @@
+import time
+import random
+
+class Man:
+            
+    def Solve_Task (self):
+        print ('Not yet ready')
+
+class Pupil(Man):
+           
+    def Sleep_Method (self, sleep):
+        self.sleep=time.sleep(randint)
+        super(Man, self).self.Sleep_Method(self, sleep)
+        
+    def Random_Method (self, randnit):
+        self.randint=random.uniform(3, 6)
+        super(Man, self).self.Random_Method(self, randnit)
+                
+vivod_Man=[Man()]
+for vivod in vivod_Man:
+    print(vivod.Solve_Task())
