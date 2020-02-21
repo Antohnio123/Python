fd9a253c6ce9f630c0d70a0fdd8d3d7451ca2624
barrygg <michaelaivanov90@gmail.com>
2020-02-21 20:56:57 +0300

    hw's addition

--- Practice/m.ivanov/Lec6_Tasks/iterts.py
+++ Practice/m.ivanov/Lec6_Tasks/iterts.py
@@ -4,8 +4,16 @@ import itertools
 def mychain(*lists):
    return list(itertools.chain(*lists))
 
-def longwords(*word, n=5):
-    return list(itertools.filterfalse(lambda x: len(x) < n, [*word]))
+# def longwords_without_itertools(lst = None, n = 5): #Не могу понять как, используя itertools, сделать так,
+#     if lst is None:                                 #чтобы на вход функции поступал массив
+#         lst =['Hello', 'I', 'write', 'cod']         #nаким способом как в заккоментированном примере
+#     return [word for word in lst if len(word) >= n]
+#
+# lst = ['aaaaa', 'aa', 'aaaaaa']
+# print(longwords_without_itertools(lst))
+
+def longwords(*words, n=5):
+    return list(itertools.filterfalse(lambda word: len(word) < n, [*words]))
 
 def combination(str, n=5):
     return list(itertools.combinations(str, n))
@@ -13,4 +21,7 @@ def combination(str, n=5):
 print(mychain([1, 2, 3], [4, 5], [6, 7]))
 print(combination('password', 5))
 print(longwords('hello', 'i', 'write', 'code'))
+
+
+
 
--- Practice/m.ivanov/Lec6_Tasks/multiplier.py
+++ Practice/m.ivanov/Lec6_Tasks/multiplier.py
@@ -3,7 +3,7 @@
 #     for i, e in enumerate(source):
 #         result[i] *= m
 #     return result
-# Ошибки:
+# Mistakes:
 # 1. Mutable Default Argument
 # print(multiplier(3))
 # [3, 6, 9]
@@ -18,7 +18,10 @@ def multiplier(source = None, m = 1):
     result = list(map(lambda x: x*m, source))
     return result
 
-print(multiplier())
-
+print(multiplier(None, 3))
+print(multiplier(None, 5))
+print(multiplier([2, 4, 6, 8], 2))
+print(multiplier([2,3,4]))
+#print(multiplier(2))
 
 
