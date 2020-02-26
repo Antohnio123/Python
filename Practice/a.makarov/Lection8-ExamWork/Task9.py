6a24bfb1b25801c062622de6e3894d407e8a7ca0
Anton <antohnio@mail.ru>
2020-02-26 20:27:55 +0300

    Task 9 is done

---
+++ Practice/a.makarov/Lection8-ExamWork/Task9.py
@@ -0,0 +1,27 @@
+class User:
+    name = "Unknown"
+    age = 0
+    def setName (self, name):
+        self.name = name
+    def getName (self):
+        return self.name
+    def setAge (self, age):
+        self.age = age
+    def getAge (self):
+        return self.age
+class Worker (User):
+    salary = 0
+    def setSalary  (self, salary):
+        self.salary=salary
+    def getSalary (self):
+        return self.salary
+John = Worker()
+John.setName("John")
+John.setAge(25)
+John.setSalary(1000)
+Jack = Worker()
+Jack.setName("Jack")
+Jack.setAge(26)
+Jack.setSalary(2000)
+SumSalary = Jack.getSalary() + John.getSalary()
+print (SumSalary)
