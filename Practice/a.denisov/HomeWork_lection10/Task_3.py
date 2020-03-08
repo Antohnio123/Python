536fffc3b1d7faebe9688f9a64ffd4b008a1a131
Denisov-AA <for.ucoz@mail.ru>
2020-03-08 14:24:45 +0300

    Homework_lection10

--- Practice/a.denisov/HomeWork_lection10/Task_3.py
+++ Practice/a.denisov/HomeWork_lection10/Task_3.py
@@ -1,29 +1,25 @@
 import urllib.request
 
-req = urllib.request.Request('http://google.com')
+req = urllib.request.Request('http://ya.ru')
 response = urllib.request.urlopen(req)
 web_page = response.read()
-
 
 
 def url_check(web_page):
     start = 0
     while web_page:
+        url_start = str(web_page).find("://", start)
+        url_end = str(web_page).find('"', url_start)
+        start = url_end
+        finded_url = str(web_page)[url_start:url_end]
         try:
-            url_start = str(web_page).find("://", start)
-            url_end = str(web_page).find('"', url_start)
-            start = url_end
-            finded_url = str(web_page)[url_start:url_end]
-            web_url = urllib.request.Request("http"+finded_url, method='HEAD')
-            if web_url.unverifiable is True:
-                print(f"URL: http{finded_url} is BAD")
-            else:
-                print(f"URL: http{finded_url} is OK")
-
+            web_url = urllib.request.urlopen("http"+finded_url)
+            if web_url.getcode() == 200:
+                print(f"http{finded_url} - good")
         except ValueError:
             break
         except Exception as text:
-            print(text)
+            print(f"http{finded_url} - {text}")
             pass
 
 
