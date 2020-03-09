af9d5e04eb64a770f4633e378ba10c2b97fc2382
Anton <antohnio@mail.ru>
2020-03-09 13:33:58 +0300

    Task3-Parser has been added.  Works with re library, deals with absolute, relative links and anchors

--- Practice/a.makarov/test.py
+++ Practice/a.makarov/test.py
@@ -1,5 +1,19 @@
-print ("Hey")
-a=4
-b=5
-c = a+b
-print (c)
+import re
+
+
+s = '.../sfgdfgdfg'
+match = re.match(r'^(\.)+/', s)
+
+
+page = 'http://google.com/users/common/native/drug/'
+
+url='..../contacts/career/jobs'
+dots = len(re.findall('\.', url.split('/')[0]))
+root = '/'.join(page.split('/')[:dots:])
+url='/'.join([root,'/'.join(url.split('/')[1:])])
+if match:
+    print('Вхождение засечено, количество точек = '+ str(hight))
+    print(root)
+    print (url)
+else:
+    print('Вхождения НЕТ!')
